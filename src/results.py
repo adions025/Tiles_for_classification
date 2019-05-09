d0f2449162a3b6634649423d8dac0fea65980e56
Adonis González <adions025@gmail.com>
2019-05-09 14:18:16 +0200

    mAP done

--- src/results.py
+++ src/results.py
@@ -8,14 +8,14 @@ Written by Adonis Gonzalez
 """
 import sys
 import os
+import matplotlib.pyplot as plt
+import numpy as np
 
 #PATHS
 ROOT_DIR = os.path.abspath("../")
 sys.path.append(ROOT_DIR)
 tiles_folder = os.path.join(ROOT_DIR, "dataset/results")
 print(tiles_folder)
-
-
 
 def read_csv_file(path):
     imgs_list = open(path + '/results.csv', 'r').readlines()
@@ -26,21 +26,96 @@ def list_dirs(path):
     subdirs = []
     for subdir in os.listdir(path):
         subdirs.append(subdir)
-        print(subdir)
     return subdirs
+
 
 
 
 if __name__ == "__main__":
 
     subdirs = list_dirs(tiles_folder)
+    mAP_calculate = 0.0
+    dict_precision = {}
 
     for subdir in subdirs:
         subdir_fullpath = os.path.join(tiles_folder, subdir)
-        print(subdir_fullpath)
-
         imgs_list = read_csv_file(subdir_fullpath)
-        print(imgs_list)
-        break
+
+        print(len(imgs_list))
+
+        tp = 0
+        fp = 0
+
+        for img in imgs_list:
+            img_name = img.strip().split('/')[-1]
+            only_prediction = (img_name.split(' - ')[1])
+            only_label = (only_prediction.split(' : ')[0])
+            #---------------------------------------------
+            # Because of the name folder
+            if subdir == 'B&C':
+                if only_label == 'b c':
+                    only_label = 'B&C'
+
+            if subdir == 'Erosion':
+                if only_label == 'erosion':
+                    only_label = 'Erosion'
+
+            if subdir == 'no_damage':
+                if only_label == 'no damage':
+                    only_label = 'no_damage'
+
+            if subdir == 'SD':
+                if only_label == 'sd':
+                    only_label = 'SD'
+
+            if subdir == 'Dirt':
+                if only_label == 'dirt':
+                    only_label = 'Dirt'
+            # ---------------------------------------------
+
+            #counting TP, FP
+            if subdir == only_label:
+                print("this one", only_label)
+                tp = tp + 1
+            else :
+                print("this no: ", only_label)
+                fp = fp + 1
+            #print (only_label)
+
+        print("---------------------------")
+        print("TP, FP",tp, fp)
+        precision = (float(tp) / float(tp +fp))
+        float(precision)
+        dict_precision.update({subdir:precision})
+        print("---------------------------")
+
+        mAP_calculate = mAP_calculate +  precision
+        float(mAP_calculate)
+
+    print("this is mAP_calculate: ", mAP_calculate)
+    print(mAP_calculate)
+    mAP = mAP_calculate / float(len(subdirs))
+    float(mAP)
+
+    print(mAP)
+
+    print(dict_precision)
+
+    labels, total = [], []
+    for key, item in dict_precision.items():
+        labels.append(key)
+        total.append(item)
+
+    x = np.arange(5)
+    plt.bar(x, height=total)
+    plt.xticks(x + .5, labels)
+    # plt.show()
+    plt.savefig(tiles_folder + '/' + 'mAP.png')
+    print("---saving----")
+
+
+
+
+
 
 
