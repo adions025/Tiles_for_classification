86eda733964bfcb6647168de6c8fff87d90d218f
Adonis González <adions025@gmail.com>
2019-05-13 14:08:58 +0200

    use this file to generate folder for each release linux and MacOSX

---
+++ versionsLinux.sh
@@ -0,0 +1,17 @@
+pwd 
+
+mkdir ../just_tiles
+git clone .git ../just_tiles
+cd  ../just_tiles
+git checkout ClassificationIN1.0
+cd ../tile_for_classification
+
+
+mkdir ../inception_with_results
+git clone .git ../inception_with_results
+cd  ../inception_with_results
+git checkout ClassificationIN1.1
+cd ../tile_for_classification
+
+
+read -n1 -r -p "Press any key to continue..." key
