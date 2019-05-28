99e58a204a23e158408b5d3e40c3ce6a290bb6a8
Adonis González <adions025@gmail.com>
2019-05-28 15:36:04 +0200

    also xml file need to be there

--- dataset/splitbeforetile.py
+++ dataset/splitbeforetile.py
@@ -19,7 +19,7 @@ folder_to_split = os.path.join(ROOT_DIR, "../dataset3/data_final")
 
 base_folder = os.path.join(ROOT_DIR, "../dataset3/base_folder") #folderbase
 dest_train = os.path.join(ROOT_DIR, "../dataset3/base_folder/train")#folderbase/train
-dest_test = os.path.join(ROOT_DIR, "../dataset3/base_folder/val")#folderbase/test
+dest_test = os.path.join(ROOT_DIR, "../dataset3/base_folder/test")#folderbase/test
 
 
 def grabNamesImages(path):
@@ -53,14 +53,36 @@ if __name__ == "__main__":
     indexs_test = np.random.choice(len(imgs_list), test_percentage, replace=False)
     print("random test files", indexs_test)
 
-    images = []
+    images, xml = [], []
     for image_name in imgs_list:
         img_name = image_name.strip().split('/')[-1]
+
+        only_img = (img_name.split('.jpg')[0])
+        xml_n = only_img + '.xml'
+
+        xml.append(xml_n)
         images.append(img_name)
 
     for i in indexs_test:
         file_to_copy = os.path.join(folder_to_split, images[i])
         copyfile(file_to_copy, dest_test + "/" + images[i])
+
+        xml_name = images[i].split('.jpg')[0]
+        only_xml = xml_name + '.xml'
+
+        file_to_copy_xml = os.path.join(folder_to_split, only_xml)
+        copyfile(file_to_copy_xml, dest_test + "/" + only_xml)
+
+        '''
+        exists = os.path.isfile(folder_to_split+ "/"+only_xml)
+        
+        path_xml = os.path.join(folder_to_split+ "/"+only_xml)
+        if exists:
+            copyfile(file_to_copy, dest_test + "/" + path_xml)
+        else:
+            "error"
+        # Keep presets
+        '''
 
     indexs_train = []
     for i in images:
@@ -71,6 +93,13 @@ if __name__ == "__main__":
     for i in training:
         file_to_copy = os.path.join(folder_to_split, images[i])
         copyfile(file_to_copy, dest_train + "/" + images[i])
+
+
+        xml_name = images[i].split('.jpg')[0]
+        only_xml = xml_name + '.xml'
+
+        file_to_copy_xml = os.path.join(folder_to_split, only_xml)
+        copyfile(file_to_copy_xml, dest_train + "/" + only_xml)
 
 
 
