339bbf02c5b599a349976ce765d1302981713fe4
Adonis González <adions025@gmail.com>
2019-06-05 16:46:58 +0200

    added new versioning file

--- versionsLinux.sh
+++ versionsLinux.sh
@@ -19,4 +19,10 @@ cd  ../inception_overlay_tiles
 git checkout ClassificationIN1.2
 cd ../tile_for_classification
 
+mkdir ../inception_overlay_tiles_and_four_points
+git clone .git ../inception_overlay_tiles_and_four_points
+cd  ../inception_overlay_tiles_and_four_points
+git checkout ClassificationIN1.3
+cd ../tile_for_classification
+
 read -n1 -r -p "Press any key to continue..." key
